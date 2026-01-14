b37a747021b4ecaab67d2bd71b9b2a4686404c12
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-14 21:21:50 +0700

    feat: view, create form input image

--- resources/js/pages/Products/Create.tsx
+++ resources/js/pages/Products/Create.tsx
@@ -7,6 +7,7 @@ import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head, Link, useForm } from '@inertiajs/react';
 import { CircleAlert } from 'lucide-react';
+import { useState } from 'react';
 import { route } from 'ziggy-js';
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -17,10 +18,15 @@ const breadcrumbs: BreadcrumbItem[] = [
 ];
 
 export default function Create() {
+    // State preview image, default image (noimage.png)
+    const [previewImage, setPreviewImage] = useState<string>(
+        '/storage/noimage.png',
+    );
     const { data, setData, post, processing, errors } = useForm({
         name: '',
         price: '',
         description: '',
+        image: null as File | null,
     });
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -34,9 +40,9 @@ export default function Create() {
             <div className="m-4">
                 <form
                     onSubmit={handleSubmit}
-                    className="flex flex-col m-4 max-w-xl space-y-4"
+                    className="m-4 mx-auto flex max-w-6xl flex-col space-y-4"
                 >
-                    <div className="flex flex-row gap-2 items-center justify-between mb-8">
+                    <div className="mb-8 flex flex-row items-center justify-between gap-2">
                         <Link
                             href={route('products.index')}
                             className="text-md group flex items-center justify-center gap-3 hover:underline md:text-lg"
@@ -54,10 +60,12 @@ export default function Create() {
                                     d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708z"
                                 />
                             </svg>
-                            <span className="hidden md:block text-foreground dark:text-foreground">Back</span>
+                            <span className="hidden text-foreground md:block dark:text-foreground">
+                                Back
+                            </span>
                         </Link>
                         <div className="flex items-center justify-start">
-                            <h1 className="text-lg font-semibold text-foreground dark:text-foreground md:text-xl">
+                            <h1 className="text-lg font-semibold text-foreground md:text-xl dark:text-foreground">
                                 Add Product
                             </h1>
                         </div>
@@ -65,7 +73,7 @@ export default function Create() {
 
                     {/* kalo ada eror, tampilkan alert */}
                     {Object.keys(errors).length > 0 && (
-                        <Alert variant="destructive" className='mb-6'>
+                        <Alert variant="destructive" className="mb-6">
                             <CircleAlert />
                             <AlertTitle>Warning</AlertTitle>
                             <AlertDescription>
@@ -81,41 +89,90 @@ export default function Create() {
                             </AlertDescription>
                         </Alert>
                     )}
-                    <div className="mb-4 flex flex-col gap-2">
-                        <Label htmlFor="name">Product Name</Label>
-                        <Input
-                            id="name"
-                            name="name"
-                            type="text"
-                            placeholder="input name Product"
-                            value={data.name}
-                            onChange={(e) => setData('name', e.target.value)}
-                        />
+                    <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+                        <div className="mx-auto max-w-[500px]">
+                            {previewImage && (
+                                <div className="mb-8">
+                                    <p className="mb-2 text-sm italic text-muted-foreground">
+                                        Image Preview
+                                    </p>
+                                    <img
+                                        src={previewImage}
+                                        alt="Preview"
+                                        className="rounded-lg border shadow w-full"
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        <div className="">
+                            <div className="mb-2 flex flex-col gap-2">
+                                <Label htmlFor="image">Product Image</Label>
+
+                                <Input
+                                    id="image"
+                                    type="file"
+                                    accept="image/*"
+                                    onChange={(e) => {
+                                        const file = e.target.files?.[0];
+
+                                        if (file) {
+                                            setData('image', file);
+                                            setPreviewImage(
+                                                URL.createObjectURL(file),
+                                            );
+                                        }
+                                    }}
+                                />
+                            </div>
+                            <div className="mb-4 flex flex-col gap-2">
+                                <Label htmlFor="name">Product Name</Label>
+                                <Input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    placeholder="input name Product"
+                                    value={data.name}
+                                    onChange={(e) =>
+                                        setData('name', e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div className="mb-4 flex flex-col gap-2">
+                                <Label htmlFor="price">Product Price</Label>
+                                <Input
+                                    id="price"
+                                    name="price"
+                                    type="number"
+                                    placeholder="input price Product"
+                                    value={data.price}
+                                    onChange={(e) =>
+                                        setData('price', e.target.value)
+                                    }
+                                />
+                            </div>
+                            <div className="mb-4 flex flex-col gap-2">
+                                <Label htmlFor="description">
+                                    Product Description
+                                </Label>
+                                <Textarea
+                                    value={data.description}
+                                    placeholder="input description Product"
+                                    onChange={(e) =>
+                                        setData('description', e.target.value)
+                                    }
+                                ></Textarea>
+                            </div>
+
+                            {/* Btn Submit Form */}
+                            <Button
+                                className="float-right mt-2 md:ms-0"
+                                type="submit"
+                                disabled={processing}
+                            >
+                                Add Product
+                            </Button>
+                        </div>
                     </div>
-                    <div className="mb-4 flex flex-col gap-2">
-                        <Label htmlFor="price">Product Price</Label>
-                        <Input
-                            id="price"
-                            name="price"
-                            type="number"
-                            placeholder="input price Product"
-                            value={data.price}
-                            onChange={(e) => setData('price', e.target.value)}
-                        />
-                    </div>
-                    <div className="mb-4 flex flex-col gap-2">
-                        <Label htmlFor="description">Product Description</Label>
-                        <Textarea
-                            value={data.description}
-                            placeholder="input description Product"
-                            onChange={(e) =>
-                                setData('description', e.target.value)
-                            }
-                        ></Textarea>
-                    </div>
-                    <Button className='mt-2 ms-auto md:ms-0' type="submit" disabled={processing}>
-                        Add Product
-                    </Button>
                 </form>
             </div>
         </AppLayout>
