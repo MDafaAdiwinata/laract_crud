f728e09d773a6947b9d12e7449a172ec8031b8e6
MDafaAdiwinata <dafaadi2106@gmail.com>
2026-01-15 11:47:33 +0700

    feat: Update Image, Storage Link Delete Image

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -15,21 +15,23 @@ class ProductController extends Controller
         return Inertia::render('Products/Index', compact('products'));
     }
 
-    public function create() {
+    public function create()
+    {
         return Inertia::render('Products/Create', []);
     }
 
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
         $request->validate([
             'name' => 'required|string|max:100',
             'price' => 'required|numeric|min:0',
             'description' => 'nullable|string',
             'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
         ]);
-        
+
         // Image Logic Upload
         $imagePath = null;
-        if ( $request->hasFile('image') ) {
+        if ($request->hasFile('image')) {
             $imagePath = $request->file('image')->store('products', 'public');
         }
 
@@ -45,30 +47,48 @@ class ProductController extends Controller
     }
 
     // Form Edit Data
-    public function edit(Product $product) {
+    public function edit(Product $product)
+    {
         return Inertia::render('Products/Edit', compact('product'));
     }
 
     // Update Data
-    public function update(Request $request, Product $product) {
+    public function update(Request $request, Product $product)
+    {
         $request->validate([
             'name' => 'required|string|max:100',
             'price' => 'required|numeric|min:0',
             'description' => 'nullable|string',
+            'image' => 'nullable|image|mimes:jpg,jpeg,png|max:2048',
         ]);
 
-        // Update to Database
-        $product->update([
+        $updateData = [
             'name' => $request->input('name'),
             'price' => $request->input('price'),
             'description' => $request->input('description'),
-        ]);
+        ];
+
+        // Handle upload image baru (jika ada)
+        if ($request->hasFile('image')) {
+            // Hapus image lama jika ada
+            if ($product->image) {
+                Storage::disk('public')->delete($product->image);
+            }
+
+            // Upload image baru
+            $imagePath = $request->file('image')->store('products', 'public');
+            $updateData['image'] = $imagePath;
+        }
+
+        // kirim ke database
+        $product->update($updateData);
 
         return redirect()->route('products.index')->with('message', 'Product updated Successfully');
     }
 
     // Delete data
-    public function destroy(Product $product) {
+    public function destroy(Product $product)
+    {
 
         // Jika image ada, maka hapus
         if ($product->image) {
